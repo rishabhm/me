ca84eb7f7b4d35baabea7b3efe034f0b0ff07553
Rishabh Marya <rishabhmarya@Rishabhs-MacBook-Pro.local>
2013-09-26 00:38:42 -0500

    changed socket connection to ec2 ip

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost');
+var socket = io.connect('http://54.235.176.127');
 
 $(document).ready(function(){
 	console.log('hey! the client side javascript is linked!');
